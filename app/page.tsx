e55f22a5b307bb965ed20e483a6208cf7953a26e
Travis Luong <travis@travisluong.com>
2023-12-26 15:55:33 +0800

    update title and h-screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "@/components/theme-switch";
 
 export default function Home() {
   return (
-    <div className="text-black bg-white dark:text-white dark:bg-black">
+    <div className="text-black bg-white dark:text-white dark:bg-black h-screen">
       <h1>Home</h1>
       <p>Hello, World!</p>
       <ThemeSwitch />
